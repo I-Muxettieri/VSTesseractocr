adbe9075fbe8049d24923c84201f59a6c9e2e083
Hajime Nagumo <matteo.antonini@live.com>
2023-11-07 14:05:50 +0100

    pdate

--- vsocr_v1.py
+++ vsocr_v1.py
@@ -1,10 +1,12 @@
 import os
+import cv2
 import sys
 import pytesseract
 import vapoursynth as vs
 import numpy as np
+from PIL import Image
 from PyQt6 import QtWidgets, QtCore, QtGui
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QLabel, QTextEdit, QFileDialog
+from PyQt6.QtWidgets import QApplication, QFileDialog, QMainWindow, QPushButton, QVBoxLayout, QLabel, QTextEdit, QFileDialog
 from PyQt6.QtCore import QThread, pyqtSignal, Qt
 from PyQt6.QtGui import QPalette, QColor
 
@@ -30,11 +32,47 @@ core = vs.core
 ffms2 = os.path.join(current_dir, 'vapoursynth', 'vapoursynth64', 'plugins', 'ffms2')
 core.std.LoadPlugin(path=ffms2)
 
-def detect_subtitles(frame):
-    # Use numpy to handle frame data
+def preprocess_image(image, hex_color):
+    # Controlla se hex_color è un colore esadecimale valido
+    if len(hex_color) != 6 or not all(c in '0123456789ABCDEFabcdef' for c in hex_color):
+        return image  # Se non è valido, restituisci l'immagine originale
+
+    # Converti il colore esadecimale in RGB
+    color = tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
+
+    # Converte l'array numpy in un'immagine PIL
+    pil_image = Image.fromarray(image)
+
+    # Ottieni i dati dell'immagine come una lista di tuple
+    data = list(pil_image.getdata())
+
+    # Crea una nuova lista di dati per la nuova immagine
+    new_data = []
+    for item in data:
+        # Se il colore del pixel corrisponde al colore specificato, aggiungilo alla nuova immagine
+        if item == color:
+            new_data.append(item)
+        else:
+            # Altrimenti, rendi il pixel trasparente (per le immagini RGBA) o bianco (per le immagini RGB)
+            new_data.append((0, 0, 0, 0) if len(item) == 4 else (255, 255, 255))
+
+    # Crea una nuova immagine con lo stesso formato e dimensioni dell'immagine originale
+    new_image = Image.new(pil_image.mode, pil_image.size)
+
+    # Aggiorna i dati della nuova immagine
+    new_image.putdata(new_data)
+    cv2.imwrite(f'temp/processed_frame_{new_image}.png', image)
+    return new_image
+
+def detect_subtitles(frame, hex_color):
+    # Usa numpy per gestire i dati del frame
     frame_array = np.asarray(frame[0])
-    # Use Pytesseract to recognize text directly from the numpy array
-    subtitle_text = pytesseract.image_to_string(frame_array, lang='ita')
+
+    # Pre-elabora l'immagine
+    processed_image = preprocess_image(frame_array, hex_color)
+
+    # Usa Pytesseract per riconoscere il testo direttamente dall'array numpy
+    subtitle_text = pytesseract.image_to_string(processed_image, lang='ita')
     return subtitle_text
 
 def milliseconds_to_srt_time(milliseconds):
@@ -50,10 +88,12 @@ def write_subtitle_to_srt(srt_file, index, start_time, end_time, subtitle_text):
 
 class ExtractSubtitlesThread(QThread):
     update_status = pyqtSignal(str)
+    update_progress = pyqtSignal(int)
 
-    def __init__(self, video_path):
+    def __init__(self, video_path, hex_color):
         super().__init__()
         self.video_path = video_path
+        self.hex_color = hex_color
 
     def run(self):
         srt_file_path = os.path.splitext(self.video_path)[0] + ".srt"
@@ -68,9 +108,10 @@ class ExtractSubtitlesThread(QThread):
             frame_num = video.num_frames
 
             for n in range(frame_num):
+                 # Aggiorna la barra di avanzamento
                 frame = video.get_frame(n)
                 frame_time = int(frame.props['_DurationNum'] * 1000 / frame.props['_DurationDen'])
-                subtitle_text = detect_subtitles(frame)
+                subtitle_text = detect_subtitles(frame, self.hex_color)
                 start_time = n * frame_time
                 end_time = start_time + frame_time
 
@@ -96,14 +137,14 @@ class SubtitleExtractor(QtWidgets.QMainWindow):
 
         # File selector button
         self.file_selector = QtWidgets.QPushButton("Seleziona file o cartella")
-        self.file_selector.setFixedSize(140, 25)
+        self.file_selector.setFixedSize(140, 30)
         self.file_selector.clicked.connect(self.select_file)
         self.file_selector.setStyleSheet("background-color: #2A2A2A; color: #FFFFFF; border: 1px solid #404040")
         layout.addWidget(self.file_selector)
 
         # File path display
         self.file_path_display = QtWidgets.QTextEdit()
-        self.file_path_display.setFixedSize(430, 25)
+        self.file_path_display.setFixedSize(430, 30)
         self.file_path_display.setReadOnly(False)  # Set to True if you want to prevent user editing
         self.file_path_display.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
         self.file_path_display.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
@@ -111,11 +152,19 @@ class SubtitleExtractor(QtWidgets.QMainWindow):
         self.file_path_display.setStyleSheet("background-color: #2A2A2A; color: #FFFFFF; border: 1px solid #404040")
         layout.addWidget(self.file_path_display)
 
+        # Hex color input
+        self.hex_color_input = QtWidgets.QLineEdit()
+        self.hex_color_input.setFixedSize(430, 30)
+        self.hex_color_input.setPlaceholderText("Inserisci il colore esadecimale qui")
+        self.hex_color_input.setStyleSheet("background-color: #2A2A2A; color: #FFFFFF; border: 1px solid #404040")
+        layout.addWidget(self.hex_color_input)
+
         # Extract subtitles button
-        extract_subtitle_button = QtWidgets.QPushButton("Estrai sottotitoli")
-        extract_subtitle_button.clicked.connect(self.extractSubtitles)
-        extract_subtitle_button.setStyleSheet("background-color: #2A2A2A; color: #FFFFFF; border: 1px solid #404040")
-        layout.addWidget(extract_subtitle_button)
+        self.extract_subtitle_button = QtWidgets.QPushButton("Estrai sottotitoli")
+        self.extract_subtitle_button.setFixedSize(430, 30)
+        self.extract_subtitle_button.clicked.connect(self.extractSubtitles)
+        self.extract_subtitle_button.setStyleSheet("background-color: #2A2A2A; color: #FFFFFF; border: 1px solid #404040")
+        layout.addWidget(self.extract_subtitle_button)
 
         # Add the status label at the bottom
         layout.addWidget(self.status_label)
@@ -135,7 +184,7 @@ class SubtitleExtractor(QtWidgets.QMainWindow):
         # Extract subtitles when the button is clicked
         if hasattr(self, 'video_path') and self.video_path:
             self.status_label.setText("Status: Processing...")
-            self.extraction_thread = ExtractSubtitlesThread(self.video_path)
+            self.extraction_thread = ExtractSubtitlesThread(self.video_path, self.hex_color_input.text())
             self.extraction_thread.update_status.connect(self.status_label.setText)
             self.extraction_thread.start()
         else:
